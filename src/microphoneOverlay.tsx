26a26d53bc0818d9e04bc48e301ae51260d24ef8
Alexander Maslov <steam3d@gmail.com>
2024-03-23 13:15:47 +0300

    Moved mic icon to the right corner near the profile image

--- src/microphoneOverlay.tsx
+++ src/microphoneOverlay.tsx
@@ -86,7 +86,7 @@ export const MicrophoneOverlay: VFC<{ backend: Backend }> = ({ backend }) => {
                                 });
 
                                 backend.log(devices);
-                                backend.log(`audioDeviceId ${micId} volume ${micSavedVolume})`);
+                                backend.log(`Saved: audioDeviceId ${micId} volume ${micSavedVolume})`);
 
                                 // Set mic volume
                                 await SteamClient.System.Audio.SetDeviceVolume(micId, 0, 0);
@@ -123,7 +123,7 @@ export const MicrophoneOverlay: VFC<{ backend: Backend }> = ({ backend }) => {
                     micId = -1;
                     micSavedVolume = -1;
                     setVisible(false);
-                    backend.log(`audioDeviceId ${audioDeviceId} audioType ${audioType} volume ${volume})`);
+                    backend.log(`Changed: audioDeviceId ${audioDeviceId} audioType ${audioType} volume ${volume})`);
                 }
             }
         );
@@ -138,15 +138,17 @@ export const MicrophoneOverlay: VFC<{ backend: Backend }> = ({ backend }) => {
         <>
             {visible &&
                 <div style={{
-                    height: "24px",
-                    width: "24px",
+                    height: "16px",
+                    width: "16px",
                     opacity: 1,
                     zIndex: 7002,
                     position: "fixed",
-                    padding: "4px"
+                    paddingTop: "1px",
+                    top: 0,
+                    right: 0
                 }}
                 >
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M9.79961 16.9472L12.0529 14.7005L22.0396 4.70719L20.6263 3.29385L15.9996 7.92049V5.36719C15.9996 4.30632 15.5782 3.28891 14.8281 2.53876C14.0779 1.78861 13.0605 1.36719 11.9996 1.36719C10.9387 1.36719 9.92134 1.78861 9.17121 2.53876C8.42108 3.28891 7.99961 4.30632 7.99961 5.36719V10.7005C8.00108 11.3722 8.17161 12.0326 8.49548 12.621C8.81941 13.2094 9.28621 13.7068 9.85294 14.0672L7.99961 15.8938C7.09908 15.1274 6.43732 14.1186 6.09297 12.9872L3.5663 13.7472C4.0375 15.2929 4.92832 16.6776 6.13964 17.7472L3.29297 20.6272L4.7063 22.0406L9.79961 16.9472Z" fill="#D4372B" />
                         <path d="M12.2196 17.3338L9.82628 19.7205C10.1031 19.7946 10.3835 19.8547 10.6663 19.9005V22.6738H13.3329V19.9272C14.9772 19.6674 16.5151 18.95 17.7707 17.8572C19.0263 16.7642 19.9489 15.3399 20.4329 13.7472L17.8796 12.9872C17.4997 14.207 16.7519 15.2796 15.7386 16.0577C14.7253 16.8359 13.4962 17.2816 12.2196 17.3338Z" fill="#D4372B" />
                     </svg>
