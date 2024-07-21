0d884fd7f82d46e2c16c158ab4ed07482321ac73
Alexander Maslov <steam3d@gmail.com>
2024-07-21 11:26:49 +0300

    Added es-ES, pt-BR

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -7,6 +7,8 @@ import ruRU from "./locales/ru-RU.json";
 import zhCN from "./locales/zh-CN.json";
 import frFR from "./locales/fr-FR.json";
 import deDE from "./locales/de-DE.json";
+import esES from "./locales/es-ES.json";
+import ptBr from "./locales/pt-BR.json";
 
 i18n
   .use(initReactI18next)
@@ -22,6 +24,8 @@ i18n
       "zh-CN": zhCN,
       "fr-FR": frFR,
       "de-DE": deDE,
+      "es-ES": esES,
+      "pt-BR": ptBr,
     },
     //debug: true,
     lng: navigator.language,
